a05abea48b98e45f81c88174392a5317307d5c64
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-26 06:01:19 +0200

    Add check and next answer functionality

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,34 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // users answer
+      const answer = e.currentTarget.value;
+      // check answer against the correct answer
+      const correct = questions[number].correct_answer === answer;
+      // add score if answer is correct
+      if (correct) setScore((prev) => prev + 1);
+      // save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    // move on to the next question if not the last question
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div>
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -25,7 +25,7 @@ const QuestionCard: React.FC<Props> = ({
     <div>
       {answers.map((answer, i) => (
         <div key={i}>
-          <button disabled={userAnswer} onClick={callback}>
+          <button disabled={userAnswer} value={answer} onClick={callback}>
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
         </div>
